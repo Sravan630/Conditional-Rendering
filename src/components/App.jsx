55bece7df2eb127e4b2c027c8eeadfcd54e9f09d
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-12-04 17:28:55 +0530

    Complete Conditional Rendering Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Form from "./Form";
 
+var registered = true;
+
 var loggedIn = true;
-
 var currentTime = new Date().getHours();
-
 function App() {
   return (
     <div className="container">
-      {currentTime > 12 && loggedIn ? <h1>Why are you still working?</h1> : <Form/>}
-      
+      {currentTime > 12 && loggedIn ? <h1>Why are you still working?</h1> : <Form  isRegistered={registered}/>}
+   
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Input from "./Input";
 
-function Form() {
+function Form(props) {
     return <form className="form">
                 <Input type="text" placeholder="Username" />
                 <Input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
+                {props.isRegistered ? null : <Input type="password" placeholder="Confirm Password" />}
+                <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>;
 }
 
