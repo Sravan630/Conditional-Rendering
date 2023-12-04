8409c6b37d28076b4c059d7a6c4d22863826c5d3
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-12-04 16:56:06 +0530

    Add files amd modifictions for Conditional Rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,16 @@
-import Card from "./Card";
-import contacts from "../contacts";
-import Heading from "./Heading";
- import Avatar from "./Avatar";
-
- function createCard(contact) {
-    return <Card 
-            key={contact.id}
-            name={contact.name}
-            imgsrc={contact.imgURL}
-            tel={contact.phone}
-            email={contact.email}
-            uid={contact.id}
-    />;
- }
+import React from "react";
 
 function App() {
-    return <div>
-        <Heading className="heading" />
-        <Avatar imgsrc={"https://picsum.photos/300"} />
-
-        {contacts.map(createCard)}
-
-
-        {/* <Card 
-            name={contacts[0].name}
-            imgsrc={contacts[0].imgURL}
-            tel={contacts[0].phone}
-            email={contacts[0].email}
-        />
-        <Card 
-            name={contacts[1].name}
-            imgsrc={contacts[1].imgURL}
-            tel={contacts[1].phone}
-            email={contacts[1].email}
-        />
-        <Card 
-            name={contacts[2].name}
-            imgsrc={contacts[2].imgURL}
-            tel={contacts[2].phone}
-            email={contacts[2].email}
-        /> */}
-    </div>;
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
-
-// function App() { 
-//     let customStyles = {
-//         color : "red"
-//     };
-
-//     let today = new Date();
-//     let t = today.getHours();
-//     let greet;
-//     if(t>=0 && t<=12) greet="Good Morning";
-//     else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
-//     else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
-//     return <div>
-//         <h1 className="greetHeading" style={customStyles}>{greet}</h1>
-//     </div>
-// };
-
-// export default App;
-
-// import Heading from "./Heading";
-// import List from "./List";
-
-// function App() {
-//     return <div>
-//         <Heading/>
-//         <List/>
-//         </div>;
-// }
-
-// export default App;
-
--- src/components/Avatar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Avatar(props) {
-    return <img src={props.imgsrc} className="circle-img" alt=""/>;
-}
-
-export default Avatar;
